1bf64df342e76025e2bebfe35cf9cf41c5a72453
James Dobbs <dobbs282@gmail.com>
2021-07-27 17:08:21 -0400

    added data to text after search is input

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,11 @@ var apiKey = "eb2134244e7565b6b1125ba38666226f";
 var searchBar = $("#searchBar");
 var searchBtn = $("#searchBtn");
 
+var uvLocalStorage = localStorage.getItem("UVIndex");
+var cityLocalStorage = localStorage.getItem("CityName");
+
 searchBtn.on("click", function (event) {
+  $("#uvIndex").removeClass();
   event.preventDefault();
   if (cityName !== "") {
     var cityName = searchBar.val().trim();
@@ -12,30 +16,63 @@ searchBtn.on("click", function (event) {
       cityName +
       "&units=imperial&appid=" +
       apiKey;
-    console.log(cityName);
-    console.log(apiCall);
     fetch(apiCall)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
-        // Pull needed Info
+        // //Pull needed Info
+        localStorage.setItem("CityName", cityName);
+        $("#previousCity").append(
+          `<li class="list-group-item"> ${cityName} </li>`
+        );
         // City Name
-        console.log("City Name: " + data.city.name);
+        var city = data.city.name;
         // Date
         var currentDate = new Date(data.list[0].dt * 1000).toLocaleDateString();
-        console.log("Date: " + currentDate);
+        $("#cityDate").text(`${city} (${currentDate})`);
         // Icon
-        console.log("Icon: " + data.list[0].weather[0].icon);
+        var icon = data.list[0].weather[0].icon;
+        document.getElementById("icon").src = `./Assets/Images/${icon}.png`;
+        document.getElementById("icon").alt = icon + " Icon";
         // Temp
-        console.log("Temp: " + data.list[0].main.temp);
+        var temp = data.list[0].main.temp;
+        $("#temp").text(`Temp: ${temp} F`);
         // Humidity
-        console.log("Humidity: " + data.list[0].main.humidity);
+        var humid = data.list[0].main.humidity;
+        $("#humidity").text(`Humidity: ${humid}%`);
+
         // Wind Speed
-        console.log("Wind Speed: " + data.list[0].wind.speed);
+        var windSpeed = data.list[0].wind.speed;
+        $("#windSpeed").text(`Wind Speed: ${windSpeed} MPH`);
         // UV Index
-        console.log("UV Index: " + data.city.name);
+        // Build the url for UV Index need lon and lat
+        var lt = data.city.coord.lat;
+        var ln = data.city.coord.lon;
+
+        var ucIndexUrl =
+          "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+          apiKey +
+          "&lat=" +
+          lt +
+          "&lon=" +
+          ln;
+        fetch(ucIndexUrl)
+          .then(function (response2) {
+            return response2.json();
+          })
+          .then(function (data2) {
+            $("#uvIndex").text(`UV Index: ${data2.value}`);
+            localStorage.setItem("UVIndex", data2.value);
+            if (data2.value < 5) {
+              $("#uvIndex").addClass("btn-success");
+            } else if (data2.value > 7) {
+              $("#uvIndex").addClass("btn-danger");
+            } else {
+              $("#uvIndex").addClass("btn-warning");
+            }
+          });
+        // CREAT BUILD PLACE 5-DAY-Forecast
       });
   }
 });

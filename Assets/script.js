61b44c3f520ada33979a8638b7918425752e9426
James Dobbs <dobbs282@gmail.com>
2021-07-27 08:58:34 -0400

    added fetch and getting a good response

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+var apiKey = "eb2134244e7565b6b1125ba38666226f";
+
+var searchBar = $("#searchBar");
+var searchBtn = $("#searchBtn");
+
+searchBtn.on("click", function (event) {
+  event.preventDefault();
+  if (cityName !== "") {
+    var cityName = searchBar.val().trim();
+    var apiCall =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityName +
+      "&units=imperial&appid=" +
+      apiKey;
+    console.log(cityName);
+    console.log(apiCall);
+    fetch(apiCall)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+});
